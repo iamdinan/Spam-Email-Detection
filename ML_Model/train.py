0573457ce7fc992f2c1087862b99223e125f70e2
Dinan <dinandilmith58@gmail.com>
2026-06-16 10:33:05 +0530

    refactor: train the model using a real dataset

--- ML_Model/train.py
+++ ML_Model/train.py
@@ -1,30 +1,48 @@
-# train.py
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
+import pandas as pd
 import joblib
 
-# small dataset (for learning)
-data = [
-    ("win money now", "spam"),
-    ("free iphone offer", "spam"),
-    ("hello how are you", "not spam"),
-    ("let's meet tomorrow", "not spam"),
-    ("claim your prize", "spam"),
-    ("project meeting at 5", "not spam")
-]
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
-texts = [x[0] for x in data]
-labels = [x[1] for x in data]
 
-# pipeline = vectorizer + model
+# load dataset
+df = pd.read_csv("spam_mail_dataset.csv")
+
+# drop missing values
+df = df.dropna(subset=["Message"])
+
+X = df["Message"]
+y = df["Category"]
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Create pipeline
 model = Pipeline([
-    ("vectorizer", CountVectorizer()),
+    ("tfidf", TfidfVectorizer()),
     ("classifier", MultinomialNB())
 ])
 
-model.fit(texts, labels)
+# Train
+model.fit(X_train, y_train)
 
+# Test
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+
+print(f"Accuracy: {accuracy:.2%}")
+print(df["Category"].unique())
+
+# Save model
 joblib.dump(model, "spam_model.pkl")
 
-print("Model trained and saved!")
+print("Model saved!")
